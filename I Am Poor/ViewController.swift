12f29611250b3845aefad9e2a47dc026ab2caae4
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2022-12-28 18:48:18 +0100

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Joshua Scherer on 28.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
